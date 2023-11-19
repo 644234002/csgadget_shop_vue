9f61bb99032ae589a091a8528946bb501128b80f
Faqih <644234002@parichat.skru.ac.th>
2023-11-19 22:51:18 +0700

    CS_Shop

---
+++ src/views/FrontEnd/Product.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2 class="fw-bold mb-4 mt-3 text-center">รายการสินค้า</h2>
+  
+    <BookList />
+  </template>
+  
+  <script setup >
+  
+  import BookList from "@/components/FrontEnd/ProductList.vue"
+  
+  
+  </script>
+  
+  <style>
+  </style>
